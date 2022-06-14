1a2ff06c1ed45152ce1ee224b19ed8f8f6e35083
Phuc Phung <phungthienphuc18@gmail.com>
2022-06-14 11:23:24 +0700

    update code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -13,25 +14,29 @@ const (
 
 func main() {
 
-	hook, _ := github.New(github.Options.Secret(""))
+	hook, _ := github.New(github.Options.Secret("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"))
 
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		payload, err := hook.Parse(r, github.RepositoryEvent)
+
+		j, _ := json.MarshalIndent(payload, "", "\t")
+		fmt.Println(string(j))
+
 		if err != nil {
+			fmt.Printf("%+v", err)
 			if err == github.ErrEventNotFound {
 				// ok event wasnt one of the ones asked to be parsed
+				fmt.Printf("Not found well well ")
 			}
 		}
 
 		switch payload.(type) {
-		case github.ReleasePayload:
-			release := payload.(github.ReleasePayload)
-			fmt.Printf("%+v", release)
-		case github.PullRequestPayload:
-			pullRequest := payload.(github.PullRequestPayload)
-			fmt.Printf("%+v", pullRequest)
+		case github.EventSubtype:
+			eventSubType := payload.(github.EventSubtype)
+			fmt.Printf("%+v", eventSubType)
 		}
 	})
 
+	fmt.Printf("Server is running at port 3000\n")
 	http.ListenAndServe(":3000", nil)
 }
